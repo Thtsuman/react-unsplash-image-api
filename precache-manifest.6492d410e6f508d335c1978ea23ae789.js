49f4aad9027edc5e90c93da6e84c91978f90d5a6
Suman Chakraborty <sumanc820@gmail.com>
2019-05-27 23:03:41 +0530

    Updates

---
+++ precache-manifest.6492d410e6f508d335c1978ea23ae789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b65ffb9d208cd4c124aa58c09dc4b5e",
+    "url": "/react-unsplash-image-api/index.html"
+  },
+  {
+    "revision": "76a4c0f6bc670e3b53d3",
+    "url": "/react-unsplash-image-api/static/css/main.c4c7d399.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/react-unsplash-image-api/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "76a4c0f6bc670e3b53d3",
+    "url": "/react-unsplash-image-api/static/js/main.49ed2205.chunk.js"
+  },
+  {
+    "revision": "89ebf23a49889381870e",
+    "url": "/react-unsplash-image-api/static/js/runtime~main.504cb890.js"
+  }
+]);
